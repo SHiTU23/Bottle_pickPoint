1b3e25d2d73be4d1337868c53ced5ecfce054d5a
SHiTU23 <shi.tu2323@gmail.com>
2024-11-21 17:28:06 +0330

    Add: class for MQTT communication

---
+++ realtime_test/mqtt_send_data.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import time
+
+class mqtt_communication:
+    # MQTT Setup
+    broker_address = "192.168.125.201"  # Localhost, change if needed
+    broker_port = 1883
+    x_coord_topic = "coord_X"
+    y_coord_topic = "coord_Y"
+    orientation_topic = "Orientation"
+    bottle_type_topic = "BottleColor"
+    response_topic = "ResponseToPLC"
+
+    def __init__(self):
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.connect(self.broker_address, self.broker_port)
+
+    def send_response_message(self):
+        message = "I have found a bottle"
+        self.mqtt_client.publish(self.response_topic, message)
+        print(f"Sent response message: {message}")
+        time.sleep(1)
+
+    def send_bottle_data(self, x, y, orientation, color):
+        self.mqtt_client.publish(self.x_coord_topic, x)
+        self.mqtt_client.publish(self.y_coord_topic, y)
+        self.mqtt_client.publish(self.orientation_topic, orientation)
+        self.mqtt_client.publish(self.bottle_type_topic, color)
+
+        print(f"Data sent:\n  X: {x}\n  Y: {y}\n  Orientation: {orientation}\n  Color: {color}")
